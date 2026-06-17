99a367a47938746f4ddfaa84ff8d9caefe210b54
Roshan Karki <roshankarki4956@gmail.com>
2026-06-17 15:59:09 +0545

    learning the artmetetic operators in  reivison of js

--- html css js revision/script.js
+++ html css js revision/script.js
@@ -1,2 +1,27 @@
 console.log("Hello World");
 
+var a = 12
+
+let b = 2.3
+const c = "hello"
+d = hello
+e = d
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+console.log(typeof(e))
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+console.log(a+b)
+
+
+console.log(a%b)
+
+var nice = confirm("Are you dumb?")
+
+
