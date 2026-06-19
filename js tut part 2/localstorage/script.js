b15c36c44110e1bedb99e48654af8a5fc5045664
Roshan Karki <roshankarki4956@gmail.com>
2026-06-19 21:11:31 +0545

    revising about local storage

--- js tut part 2/localstorage/script.js
+++ js tut part 2/localstorage/script.js
@@ -6,3 +6,6 @@ localStorage.setItem("Name: ", "Roshan")
 
 Name = localStorage.getItem("Name")
 
+localStorage.setItem("friends",JSON.stringify(["Rojika","Diya","Sesehang"]))
+
+// ARRAYS DONT WORK ON LOCAL STORAGE SO WE STRINGIFY IT 
