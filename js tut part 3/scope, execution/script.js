4a890e12917de9dfd723c763a1b9d3079a8b7652
Roshan Karki <roshankarki4956@gmail.com>
2026-06-19 22:42:47 +0545

    Learning about lexical and dynaimc scoping

---
+++ js tut part 3/scope, execution/script.js
@@ -0,0 +1,31 @@
+// Execution Content
+
+
+// Memory Phase
+
+// Execution Phase
+
+
+
+function abcd() {
+    
+    var a = 12;
+    var b = 13;
+    var c = 14;
+
+}
+
+
+// Lexical Scope
+
+
+function defg() {
+    let a = 12;
+    function ghij() {
+        console.log(a);
+    }
+}
+
+// Dynamic Scope  This is not followed by any programming languages as per now 
+
+ 
