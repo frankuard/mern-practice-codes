3f3b138d4dfdbe5c3208f8451b5356241fa5de25
Roshan Karki <roshankarki4956@gmail.com>
2026-06-20 22:34:07 +0545

    Learning about this keyword

---
+++ js tut part 3/call apply bind/script.js
@@ -0,0 +1,27 @@
+// Call
+
+let obj = {
+    name: "roshan",
+    age: 12
+}
+
+function abcd(a,b,c) {
+    console.log(this.age, a,b,c);
+
+}
+
+abcd.call(obj, 1,2,3);
+
+
+// Calling abcd with obj as parameter
+
+// Bind
+
+abcd.apply(obj,[1,2,3]) // Bind only accepts two parameter  (Used Rarely)
+
+
+let fnc = abcd.bind(obj,1,2,3); // Copies function 
+fnc(); // The obj becomes the main one
+
+
+
