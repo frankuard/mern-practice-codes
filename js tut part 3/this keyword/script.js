5d2bb07110ef0ce000788cb1528927ec85b744fb
Roshan Karki <roshankarki4956@gmail.com>
2026-06-20 12:00:29 +0545

    learning about global scope function scope and method scope in this keyword

--- js tut part 3/this keyword/script.js
+++ js tut part 3/this keyword/script.js
@@ -4,7 +4,7 @@
 
 console.log(this);
 
-
+// FUNCTION SCOPE
 
 function abcd() {
     console.log(this);
@@ -16,9 +16,37 @@ console.log(abcd());
 
 
 
+// METHOD SCOPE
+
 let obj = {
     name: "Roshan",
+    age : 18,
     sayName: function () {
-        console.log(this);
-    }
+        console.log(this.age);
+    },
 };
+
+obj.sayName();  // If this was arrow function then this will loose its value because its es6 also if there is nested function then it will also loose its value 
+
+
+// Event Handling 
+
+document.querySelector("h1").addEventListener("click", function () {
+    console.log(this.style.color = "red")
+})
+
+
+// NOTE : 
+// global  is window  
+// function is window
+// method with es5 function is object 
+// method with es6 arrow function is window
+// es5 function inside es5 function is window
+// arrow function inside es5 function is object
+// event handler is Element
+// class is blank object
+
+
+
+
+
