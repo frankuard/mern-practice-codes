37fead552556f2fb04ff6369edeac5a8861e41d5
Roshan Karki <roshankarki4956@gmail.com>
2026-06-20 22:46:28 +0545

    Learning about practical implementation about this keyword

--- js tut part 3/call apply bind/script.js
+++ js tut part 3/call apply bind/script.js
@@ -24,12 +24,16 @@ let fnc = abcd.bind(obj,1,2,3); // Copies function
 fnc(); // The obj becomes the main one
 
 
-
+let form  = document.querySelector("form");
 
 const userManager = {
 
     users : [],
-    init: function () { alert(); },
+    init: function () { form.addEventListener("submit", this.submitForm.bind(this))},
+    submitForm:  function (e) {
+        e.preventDefault(),
+        console.log("Form Submitted!")
+    },
     addUser: function () {},
     removeUser: function () {},
 
