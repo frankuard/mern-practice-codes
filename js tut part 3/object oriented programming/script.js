13fa6983c73b14115b3d6b4d030e729d8590ef26
Roshan Karki <roshankarki4956@gmail.com>
2026-06-21 13:45:00 +0545

    learning about constructor functions

--- js tut part 3/object oriented programming/script.js
+++ js tut part 3/object oriented programming/script.js
@@ -1,2 +1,11 @@
 // Classes and Objects
 
+function createPencil(name,price,color,company) {
+    this.Realname  = name;
+    this.Realprice = price;
+    this.Realnamecolor = color;
+    this.Realcompany = company;
+}
+
+let pencil1 = new createPencil("Nataraj",10,"Black","Nataraj");
+
