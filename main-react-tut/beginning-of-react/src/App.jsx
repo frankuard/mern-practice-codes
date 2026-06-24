415457c6c05a3ccb38c8358deea75d03a826c3d0
Roshan Karki <roshankarki4956@gmail.com>
2026-06-24 15:28:31 +0545

    learning about react inntial step

---
+++ main-react-tut/beginning-of-react/src/App.jsx
@@ -0,0 +1,18 @@
+  import React from 'react'
+
+const App = () => {
+  return (
+    <>
+
+    <div className="dad">
+hello world
+    </div>
+<div className="chacha">
+hi bro
+</div>
+    </>
+  )
+}
+
+export default App
+
