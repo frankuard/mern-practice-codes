159893220194328aef24fab6570d7079ae781c22
Roshan Karki <roshankarki4956@gmail.com>
2026-08-02 02:27:03 +0545

    learning about the rest apis and crud operations on the post method and also understood the differnces between post and get and also knoew that it is from fromt end perspective

---
+++ backend-tutorials/apis-restApis/server.js
@@ -0,0 +1,9 @@
+// To start the server
+
+const app = require('./src/app');
+
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+})
---
+++ backend-tutorials/apis-restApis/src/app.js
@@ -0,0 +1,23 @@
+// To create server
+
+const express = require('express');
+
+
+const app = express()
+
+app.use(express.json());
+
+const notes = [
+
+]
+
+app.post('/notes',(req,res) =>{
+
+    notes.push(req.body)
+
+    res.status(201).json({message: "Notes Created Successfully"})
+})
+
+
+
+module.exports = app
