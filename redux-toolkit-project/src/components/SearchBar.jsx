368a8d96d72be428a146b904ee3e9f26acba06d8
Roshan Karki <roshankarki4956@gmail.com>
2026-08-01 23:56:27 +0545

    editing somewhat half of the logic for the searchbar and bujttons

---
+++ redux-toolkit-project/src/components/SearchBar.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const SearchBar = () => {
+
+    const [text, setText] = useState('')
+
+    const submitHandler = (e) => {
+        e.preventDefault()
+        console.log("form submited");
+    }
+
+  return (
+    <div>
+        <form onSubmit={(e) =>{
+                submitHandler(e)
+        }} className=' flex gap-5 bg-gray-900 p-10 '>
+            <input 
+            value={text}
+            onChange={(e) => {
+                setText(e.target.value)
+            }}
+            required
+            className=' w-full border-2 px-4 py-2 text-xl rounded outline-none'
+            type="text" placeholder='Search anything...'/>
+            <button className=' active:scale-95 cursor-pointer border-2 px-4 py-2 text-xl rounded outline-none'>Search</button>
+        </form>
+
+    </div>
+  )
+}
+
+export default SearchBar
