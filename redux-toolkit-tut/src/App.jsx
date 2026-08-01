80e2f07f349e56a214a00a8112e3715e96f456b2
Roshan Karki <roshankarki4956@gmail.com>
2026-08-01 16:06:55 +0545

    learning redux toolki

--- redux-toolkit-tut/src/App.jsx
+++ redux-toolkit-tut/src/App.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { decrement, increment, incrementbyAmount } from './redux/features/counterSlice'
 
 const App = () => {
+
+  const dispatch = useDispatch()
+
+  const count = useSelector((state) => state.counter.value)
+
+  const [num, setNum] = useState(5)
+
   return (
-    <div>App</div>
+    <div>
+      <h1>{count}</h1>
+
+      <button onClick={() => {
+        dispatch(increment())
+      }}>Increment</button>
+
+      <button onClick={() => {
+        dispatch(decrement())
+      }}>Decrement</button>
+
+      <input type="number"
+        value={num}
+        onChange={(e) => {
+          setNum(e.target.value)
+        }} />
+
+      <button onClick={() => {
+        dispatch(incrementbyAmount(Number(num)))
+      }}>Increase by Amount</button>
+    </div>
   )
 }
 
