6a87f237104b0e03617fb61a542defdd61bb1b04
Roshan Karki <roshankarki4956@gmail.com>
2026-08-02 13:51:13 +0545

    integrqating and connecting database to server

---
+++ backend-tutorials/databases/src/db/db.js
@@ -0,0 +1,13 @@
+const dns = require('dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']);
+
+const mongoose = require('mongoose');
+
+async function connectDB() {
+    await mongoose.connect("mongodb+srv://roshankarki4956_db_user:I3D5Zy3Oqq5Upcmw@backend-tutorial.fppqy4w.mongodb.net/halley");
+
+    
+    console.log("Connected to DB");
+}
+
+module.exports = connectDB;
