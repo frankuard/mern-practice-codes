4d488b1aac80ff5cad6771d8e8e170b14fe69b55
Roshan Karki <roshankarki4956@gmail.com>
2026-08-08 18:10:51 +0545

    learning about how to fetch and get data from mongo db to imagekit and all

--- backend-tutorials/backend-prj/src/app.js
+++ backend-tutorials/backend-prj/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const multer = require('multer');
 const uploadFile = require('./services/storage.service');
+const postModel = require('./models/post.model');
 const app = express();
 
 app.use(express.json()) // THIS IS FOR RAW API POST
@@ -16,8 +17,25 @@ app.post('/create-post', upload.single("image"), async (req,res) => {
 
     const result = await uploadFile(req.file.buffer)
 
-    console.log(result);
+    const post = await postModel.create({
+        image: result.url,
+        caption: result.caption
+    })
+
+    return res.status(201).json({
+        message:"Post Created Successfully",
+        post
+    })
 })
 
+app.get("/posts", async (req,res) => {
+    
+    const posts = await postModel.find()
+
+    return res.status(200).json({
+        message: 'Post fetched successfully',
+        posts
+    })
+})
 
 module.exports = app
--- backend-tutorials/backend-prj/src/db/db.js
+++ backend-tutorials/backend-prj/src/db/db.js
@@ -1,12 +1,14 @@
+
 const dns = require('dns');
 dns.setServers(['8.8.8.8', '1.1.1.1']);
+
 
 const mongoose = require('mongoose');
 
 
 async function connectDB() {
     
-    await mongoose.connect("mongodb+srv://roshankarki4956_db_user:I3D5Zy3Oqq5Upcmw@backend-tutorial.fppqy4w.mongodb.net/project-1")
+    await mongoose.connect(process.env.MONGO_URI)
 
     console.log("Connected to DB");
 }
