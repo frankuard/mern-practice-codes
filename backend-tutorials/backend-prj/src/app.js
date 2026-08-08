771e28fd39acb5716b5ee2459c62dc868fa2e7ec
Roshan Karki <roshankarki4956@gmail.com>
2026-08-08 17:53:00 +0545

    learning about how storage service provider is helpful

--- backend-tutorials/backend-prj/src/app.js
+++ backend-tutorials/backend-prj/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const uploadFile = require('./services/storage.service');
 const app = express();
 
 app.use(express.json()) // THIS IS FOR RAW API POST
@@ -12,5 +13,11 @@ app.post('/create-post', upload.single("image"), async (req,res) => {
     
     console.log(req.body);
     console.log(req.file);
+
+    const result = await uploadFile(req.file.buffer)
+
+    console.log(result);
 })
+
+
 module.exports = app
