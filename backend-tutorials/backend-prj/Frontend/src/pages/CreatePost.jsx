35626424b27af79178b31052ffd8602ec81b1849
Roshan Karki <roshankarki4956@gmail.com>
2026-08-08 22:42:54 +0545

    made both frontend and backend and integrating it at the end of it and learned how to send data through backend

--- backend-tutorials/backend-prj/Frontend/src/pages/CreatePost.jsx
+++ backend-tutorials/backend-prj/Frontend/src/pages/CreatePost.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
-
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 
 const CreatePost = () => {
+
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const formData = new FormData(e.target)
+
+        axios.post("http://localhost:3000/create-post", formData)
+        .then((res) => {
+            navigate("/feed")
+        
+        })
+    }
+
   return (
    
         <section className='create-post-section'>
                 <h1>Create Post</h1>
 
-                <form>
+                <form onSubmit={handleSubmit}>
                     <input type="file" name='image' accept='image/*' />
                     <input type="text" name='caption' required placeholder='Write a Caption'/>
                     <button type='submit'> Submit</button>
--- backend-tutorials/backend-prj/Frontend/src/pages/Feed.jsx
+++ backend-tutorials/backend-prj/Frontend/src/pages/Feed.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 
 const Feed = () => {
 
     const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+      axios.get("http://localhost:3000/posts")
+      .then((res) => {
+        setPosts(res.data.posts)
+      })
+    }, [])
+    
   return (
     <section className='feed-section'>
             {
                 posts.length > 0 ? (
                     posts.map((post) => (
                         <div key={post._id} className='post-card'>
-                            <img src={posts.image} alt={posts.caption} />
-                            <p>{posts.caption}</p>
+                            <img src={post.image} alt={post.caption} />
+                            <p>{post.caption}</p>
                         </div>
                     ))
                 ) : (
