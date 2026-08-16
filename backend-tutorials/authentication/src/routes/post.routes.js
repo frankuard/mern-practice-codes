1013c02c86b0b6a5bf619807050ea3ec528f159d
Roshan Karki <roshankarki4956@gmail.com>
2026-08-16 10:35:01 +0545

    learning about authentication

--- backend-tutorials/authentication/src/routes/post.routes.js
+++ backend-tutorials/authentication/src/routes/post.routes.js
@@ -1,11 +1,35 @@
 const express = require('express');
+
+const jwt = require('jsonwebtoken');
+
+const userModel = require('../models/user.model')
+
 const router = express.Router();
 
-router.post("/create", (req,res)=> {
-    console.log(req.body);
+router.post("/create", async (req, res) => {
 
-    console.log(req.cookies);
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            messsage: "Unauthorized"
+        })
+    }
 
+    try {
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        
+
+        const user = await userModel.findOne({
+         _id: decoded.id
+        })
+
+        console.log(user);
+    } catch (err) {
+            return res.status(401).json({
+                message: "Wrong token"
+            })
+    }
     res.send("Post created successfully")
 })
 
