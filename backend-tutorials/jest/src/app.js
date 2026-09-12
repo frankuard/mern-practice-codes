6542ae1258ba99416b77b4a7d8bc2349969cc7f0
Roshan Karki <roshankarki4956@gmail.com>
2026-09-12 14:11:50 +0545

    learning about express validator

--- backend-tutorials/jest/src/app.js
+++ backend-tutorials/jest/src/app.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const { registerUserValidationRules } = require('./middlewares/validation.middleware');
 
 const app  = express();
 
 
-app.post("/register",(req,res) => {
+app.post("/register",registerUserValidationRules,(req,res) => {
 
     const {username, email, password} = req.body;
+
+    res.status(201).json({message: "User registered succesffuly", user:{
+        username,email
+    }})
 })
 
 module.exports = app;
---
+++ backend-tutorials/jest/src/middlewares/validation.middleware.js
@@ -0,0 +1,32 @@
+const { body, validationResult} = require("express-validator");
+
+
+async function validateResult(req,res,next) {
+
+    const errors = validateResult(req);
+
+    if (!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()})
+    }
+
+    next();
+}
+
+
+const registerUserValidationRules = [
+    body("username")
+    .isString()
+    .withMessage("Username must be string")
+    .isLength({min:3, max:20})
+    .withMessage("Username must be between 3 and 20 characters"),
+    body("email")
+    .isEmail()
+    .withMessage("Invalid Email Address"),
+    body("password")
+    .isLength({min:6})
+    .withMessage("Password must be at least 6 characters long"),
+    validateResult
+]
+
+
+module.exports  = {registerUserValidationRules}
